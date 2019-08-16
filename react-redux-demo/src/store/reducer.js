e77c409a41ebc85dcd8a9c0b9f59435a83bffc9d
LP_ZX <2295140520@qq.com>
2019-08-16 23:22:33 +0800

    test(reducer): 创建 todo reducer  模块
    根据 action  的类型来处理不同的业务操作，也是用 redux-thunk 完成异步 action

---
+++ react-redux-demo/src/store/reducer.js
@@ -0,0 +1,31 @@
+import { CHANGE_INPUT, ADD_TODO, DELETE_TODO, GET_TODO } from './actionTypes'
+
+const defaultState = {
+  inputValue: 'Write Something',
+  list: []
+}
+
+export default (state = defaultState, action) => {
+  if (action.type === CHANGE_INPUT) {
+    let newState = JSON.parse(JSON.stringify(state))
+    newState.inputValue = action.value
+    return newState
+  }
+  if (action.type === ADD_TODO) {
+    let newState = JSON.parse(JSON.stringify(state))
+    newState.list.push(newState.inputValue)
+    newState.inputValue = ''
+    return newState
+  }
+  if (action.type === DELETE_TODO) {
+    let newState = JSON.parse(JSON.stringify(state))
+    newState.list.splice(action.index, 1)
+    return newState
+  }
+  if (action.type === GET_TODO) {
+    let newState = JSON.parse(JSON.stringify(state))
+    newState.list = action.data.data.list
+    return newState
+  }
+  return state
+}
