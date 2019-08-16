a3ce9d9b663a048f1866d87b7738b9a0f3f51956
LP_ZX <2295140520@qq.com>
2019-08-16 23:19:45 +0800

    test(store): 创建 store 仓库
    使用 redux 创建 store  仓库

---
+++ react-redux-demo/src/store/index.js
@@ -0,0 +1,20 @@
+// import { createStore } from 'redux'
+import { createStore , applyMiddleware, compose} from 'redux'
+import reducer from './reducer'
+import createSageMiddleware from 'redux-saga'
+import mySagas from './sagas'
+// import reduxThunk from 'redux-thunk'
+
+const sagaMiddleware = createSageMiddleware()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}):compose
+
+const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware))
+
+const store = createStore(
+  reducer,
+  enhancer
+)
+
+sagaMiddleware.run(mySagas)
+
+export default store
