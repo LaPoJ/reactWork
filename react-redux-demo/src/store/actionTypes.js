aa935db8102923f4c4c58cc428ca494a8afe8873
LP_ZX <2295140520@qq.com>
2019-08-16 23:18:56 +0800

    test(actions): 定义 action type
    定义所需要用到的 action type 为常量，分离成模块，使其解耦

---
+++ react-redux-demo/src/store/actionTypes.js
@@ -0,0 +1,5 @@
+export const CHANGE_INPUT = 'changeInput'
+export const ADD_TODO = 'ADD_TODO'
+export const DELETE_TODO = 'DELETE_TODO'
+export const GET_TODO = 'GET_TODO'
+export const GET_MY_LIST = 'GET_MY_LIST'
